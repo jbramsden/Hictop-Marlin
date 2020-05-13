d0d7e4a94408787af0d55dcca37038338b4dbf11
jason@bramsden.com <jason@bramsden.com>
2020-05-13 15:16:05 +0100

    Updated board to be MKS_GEN_13 which works for E1

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -128,7 +128,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_BASE //Hictop
+  #define MOTHERBOARD BOARD_MKS_GEN_13 //Hictop
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
